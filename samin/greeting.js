a69ee6cb1cfde4f2b098739dbda9f55d52c30b65
min <minalee0696@gmail.com>
2020-07-29 22:10:27 +0900

    3-5 without css

---
+++ samin/greeting.js
@@ -0,0 +1,49 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+    
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function handleSubmit(event)
+{
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function saveName(text)
+{
+    localStorage.setItem(USER_LS, text)
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+    paintGreeting(currentValue);
+}
+
+function paintGreeting(text)
+{
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function loadName()
+{ 
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null){
+        askForName();
+    } else {
+        paintGreeting(currentUser);
+    } 
+}
+
+
+function init(){
+    loadName();
+}
+
+init();
