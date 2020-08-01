c4d8a6fb32de724fbd7a3ad2aa6a40ef9f93f163
min <minalee0696@gmail.com>
2020-08-01 22:44:08 +0900

    samin 03_06

--- samin/todo.js
+++ samin/todo.js
@@ -4,16 +4,30 @@ const toDoform = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos()
+{
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 function paintToDo(text)
 {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText= "❌";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText= "❌";
     span.innerText = text;
-    li.appendChild(span);
+    li.appendChild(span); 
     li.appendChild(delBtn);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text, 
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event)
@@ -26,9 +40,12 @@ function handleSubmit(event)
 
 function loadToDos ()
 {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos === null){
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        });
     }
 }
 
