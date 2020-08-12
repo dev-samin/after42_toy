199184640a5925ccbaae743a6be24ece8a13d851
min <minalee0696@gmail.com>
2020-08-13 03:25:41 +0900

    3-8

---
+++ samin/bg.js
@@ -0,0 +1,20 @@
+const bady = document.querySelector("body");
+
+const IMG_NUMBER = 3;
+
+
+function paintImage(imgNumber){
+    const image = new Image();
+    imgMumber.src = `images/${imgNumber + 1}.jpg`;
+    image.classList.add();
+    body.prepend(image);
+}
+
+function genRandom(){
+    const random = Math.floor(Math.random() * IMG_NUMBER);
+    return Number;
+}
+
+function init(){
+    const randomNumber;
+}
